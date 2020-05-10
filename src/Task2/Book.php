a6025721ead7c11f186c62534420efcc355f0087
Evgen Zukov <searchyou13@gmail.com>
2020-05-10 11:15:11 +0300

    Resolve second task

--- src/Task2/Book.php
+++ src/Task2/Book.php
@@ -4,20 +4,61 @@ declare(strict_types=1);
 
 namespace App\Task2;
 
+/**
+ * Class Book
+ * @package App\Task2
+ */
 class Book
 {
+    /**
+     * @var string
+     */
+    private string $title;
+
+    /**
+     * @var int
+     */
+    private int $price;
+
+    /**
+     * @var int
+     */
+    private int $pagesNumber;
+
+    /**
+     * Book constructor.
+     * @param string $title
+     * @param int $price
+     * @param int $number
+     */
+    public function __construct(string $title, int $price, int $pagesNumber)
+    {
+        $this->title = $title;
+        $this->price = $price;
+        $this->pagesNumber = $pagesNumber;
+    }
+
+    /**
+     * @return string
+     */
     public function getTitle(): string
     {
-        // @todo
+        return $this->title;
     }
 
+    /**
+     * @return int
+     */
     public function getPrice(): int
     {
-        // @todo
+        return $this->price;
     }
 
+    /**
+     * @return int
+     */
     public function getPagesNumber(): int
     {
-        //@todo
+        return $this->pagesNumber;
     }
 }
